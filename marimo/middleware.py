43847157d539b9596786ce03f9300508bc8be879
Alex Dreyer <alex.dreyer@cmgdigital.com>
2012-01-06 11:39:27 -0500

    [CMSPERF-179] added code to the middleware

--- marimo/middleware.py
+++ marimo/middleware.py
@@ -11,6 +11,7 @@ class Marimo(object):
     def process_request(self, request):
         """ sticks marimo_widgets in the request """
         request.marimo_widgets = []
+        request.marimo_slow = False
 
     def process_response(self, request, response):
         """ generates a script to register and load the widgets with marimo """
@@ -21,11 +22,18 @@ class Marimo(object):
             return response
         code = "marimo.add_widgets(%s);" %json.dumps(request.marimo_widgets)
 
+        if getattr(request, 'marimo_slow', False):
+            # TODO find slow event and this should concat not replace
+            code = "marimo.widgetlib.writecapture_widget.render_events = [%s];" % SLOW_EVENT + code
+
         response.content = MARIMO_PLACEHOLDER.sub(code, response.content)
         return response
 
 def context_processor(request):
     """ sticks marimo_widgets into the template context """
+    extra_context = {}
     if hasattr(request, 'marimo_widgets'):
-        return { 'marimo_widgets' : request.marimo_widgets }
-    return {}
+        extra_context['marimo_widgets'] = request.marimo_widgets
+    if hasattr(request, 'marimo_slow'):
+        extra_context['marimo_slow'] = request.marimo_slow
+    return extra_context
