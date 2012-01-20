db18b62bb6875cc363a324828ad3eece4e33a456
cmg <cmg@cmg-virtualbox.hsd1.ga.comcast.net>
2012-01-20 10:56:24 -0500

    CMSPERF-179 - added import for middleware tests

--- marimo/tests/__init__.py
+++ marimo/tests/__init__.py
@@ -1,2 +1,3 @@
 from marimo.tests.test_views import TestRouterView, TestBaseView
 from marimo.tests.test_tags import TestTag
+from marimo.tests.test_middleware import TestMiddleware, TestContextProcessor
