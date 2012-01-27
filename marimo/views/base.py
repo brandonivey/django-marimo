dac23a46bbb74a69b09866d3cfa8f6582700de5c
Michael F. Lamb <mike@datagrok.org>
2012-01-27 14:42:26 -0500

    [LAB-20] tease apart caching and auto-template stuff

--- marimo/views/base.py
+++ marimo/views/base.py
@@ -11,39 +11,47 @@ from django.http import HttpResponse
 
 MARIMO_TIMEOUT = getattr(settings, 'MARIMO_TIMEOUT', 60*60*24)
 
-class BaseWidget(object):
+class BaseWidgetHandler(object):
     """
     Extend BaseWidget to make marimo widget handlers.
 
-    Properties::
-
-        use_cache:  If nothing is cacheable in your handler set this to false and don't define a cacheable method
-        template:   A string; the template data that this widget will use. (Not a filename.)
-
-    To load a template from a file, consider:
-
-        template = template_loader.load('my_template.html')
-
-    ... which will look for its path relative to a dir in MARIMO_TEMPLATE_DIRS.
+    If part of the data in your handler is cacheable, override cacheable() and
+    cache_key(). If no data in your handler is cacheable, don't override
+    cache_key().
 
     """
-    use_cache = True
-    template = ''
 
     # TODO: everything seems stateless should these be classmethods?
     def cacheable(self, response, *args, **kwargs):
         """
-        Updates and returns response dictionary with cacheable data.
+        Returns a response dictionary with cacheable data.
 
-        Should be overriden in subclasses if use_cache=True
+        Should be overriden in subclasses that have cacheable data.
+
+        Subclasses that override this method should also override cache_key.
+
+        Subclasses should always return the updated dict; callers should use
+        the return value. Do not rely on the dict passed in as 'response' to be
+        modified in-place.
+
+        Subclasses normally need not call this function, however callers should
+        not assume that the dict passed in as 'response' will not be modified
+        as a side-effect.
+
         """
         return response
 
     def uncacheable(self, request, response, *args, **kwargs):
         """
-        Updates response dictionary with uncacheable data.
+        Returns a response dictionary with uncacheable data.
 
         Should be overwritten in all subclasses.
+
+        Subclasses should always return the updated dict; callers should use
+        the return value. Do not rely on the dict passed in as 'response' to be
+        modified in-place. Neither should one assume that the dict passed in as
+        'response' will not be modified as a side-effect.
+
         """
         return response
 
@@ -51,25 +59,25 @@ class BaseWidget(object):
         """
         Generates the cache key that this widget will use based on  *args and **kwargs
 
-        Must be overridden in subclass if use_cache=True
+        Must be overridden in subclasses if cacheable is overridden.
+
+        A return value of 'None' is special; it means "do not cache for these
+        args/kwargs". Don't return a cache key if you don't need to cache
+        anything; otherwise the default return value of cacheable() will be
+        redundantly and wastefully cached.
         """
-        raise NotImplementedError
+        pass
 
     def get_cache(self, *args, **kwargs):
         """ get current cached cacheable part """
+        response = None
         cache_key = self.cache_key(*args, **kwargs)
-        response = cache.get(cache_key)
+        if cache_key:
+            response = cache.get(cache_key)
         if response is None:
-            response = self.update_cache(response, *args, **kwargs)
-        return response
-
-    def update_cache(self, response, *args, **kwargs):
-        """ call this with *args and **kwargs like a request to update the cache """
-        cache_key = self.cache_key(*args, **kwargs)
-        if response is None:
-            response = {'template':self.template, 'context':dict()}
-        response = self.cacheable(response, *args, **kwargs)
-        cache.set(cache_key, response, MARIMO_TIMEOUT)
+            response = self.cacheable({'context': dict()}, *args, **kwargs)
+        if cache_key:
+            cache.set(cache_key, response, MARIMO_TIMEOUT)
         return response
 
     def on_error(self, ex, data, request, *args, **kwargs):
@@ -87,15 +95,8 @@ class BaseWidget(object):
         return data
 
     def __call__(self, request, *args, **kwargs):
-        """
-        Splits up work into cachable and uncacheable parts
-        """
-
-        if self.use_cache:
-            response = self.get_cache(*args, **kwargs)
-        else:
-            response = {'template':self.template, 'context':dict()}
-
+        """ Splits up work into cachable and uncacheable parts """
+        response = self.get_cache(*args, **kwargs)
         response = self.uncacheable(request, response, *args, **kwargs)
         return response
 
@@ -108,3 +109,35 @@ class BaseWidget(object):
             response = inst(request, *data['args'], **data['kwargs'])
             return HttpResponse(json.dumps(response), mimetype='application/json')
         return view
+
+class RequestWidgetHandler(BaseWidgetHandler):
+    """Handler (abstract superclass) for (subclasses of) marimo.widgetlib.request_widget
+
+    To handle requests for your subclass of marimo.widgetlib.request_widget,
+    create a subclass of this, overriding at minimum:
+
+        template
+        uncacheable()
+
+    If some data is cacheable, override cache_key() and cacheable().
+
+    This handler assumes that all requests for your widget use the same template.
+
+    Properties::
+
+        template:   A string; the template data that this widget will use. (Not a filename.)
+
+    To load a template from a file relative to a dir in MARIMO_TEMPLATE_DIRS:
+
+        template = template_loader.load('my_template.html')
+
+    """
+    template = ''
+
+    def cacheable(self, response, *args, **kwargs):
+        response = super(RequestWidgetHandler, self).cacheable(response, *args, **kwargs)
+        response['template'] = self.template
+        return response
+
+# backwards compatibility. Once nothing else refers to BaseWidget, delete this
+BaseWidget = RequestWidgetHandler
